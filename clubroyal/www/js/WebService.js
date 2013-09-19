5294a9aaef5a9885d265c2efe74348581eeab085
Davidg <Davidg@192.168.1.180>
2013-09-19 18:49:36 -0500

    Login

--- clubroyal/www/js/WebService.js
+++ clubroyal/www/js/WebService.js
@@ -1,40 +1,30 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+var envia = function() {
 
-webService = function() {
+    jQuery.support.cors = true;
 
-    var wsUrl = "http://adccmdev.programaatusalud.com/wsdl";
-    var soapRequest =
-            '<?xml version="1.0" encoding="utf-8"?> \
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" \
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema" \
-    xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> \
-  <soap:Body> \\n\
-  <getComisionistas>\
-  </getComisionistas>\
-  </soap:Body> \
-</soap:Envelope>';
+
+    var wsUrl = "http://localhost/mapfreRe/public/wsdl/index/soap/getEstados";
+    var wsUrl = "http://programaatusalud.com/adccmdev/public/wsdl/index/soap/getComisionistas";
+    var soapRequest = '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://adccmdev.programaatusalud.com/wsdl/index/soap" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:enc="http://www.w3.org/2003/05/soap-encoding"><env:Body><ns1:getComisionistas env:encodingStyle="http://www.w3.org/2003/05/soap-encoding"/></env:Body></env:Envelope>';
+
     $.ajax({
         type: "POST",
         url: wsUrl,
-        contentType: "text/xml",
+        contentType: "application/soap+xml",
         dataType: "xml",
         data: soapRequest,
         success: processSuccess,
         error: processError
     });
 
-    function processSuccess(data, status, req) {
-        if (status === "success") {
-            console.log(data);
-            console.log(req);
-        }
-    }
 
+    function processSuccess(data, status, req) {
+        alert(status);
+        if (status == "success")
+            $("#response").text($(req.responseXML).find("HelloResult").text());
+    }
     function processError(data, status, req) {
+        console.log(data);
         alert(req.responseText + " " + status);
     }
-
 };
