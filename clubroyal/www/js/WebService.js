1e167260ff1f35accdfd323b891e8f9deca55cf0
Davidg <Davidg@192.168.1.180>
2013-09-20 12:05:58 -0500

    Login

--- clubroyal/www/js/WebService.js
+++ clubroyal/www/js/WebService.js
@@ -16,6 +16,7 @@ var canlogin = function() {
 
     function processSuccess(data, status, req) {
         $("#response").text(data);
+         alert(status);
         if (status === "success") {
             var response = $(req.responseXML).find("return").text();
             alert(response);
