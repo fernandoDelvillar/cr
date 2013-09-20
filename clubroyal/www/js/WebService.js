f9ece26c53edb430ad2dcd59da6f3c9f55b80f0f
Davidg <Davidg@192.168.1.180>
2013-09-20 11:43:00 -0500

    Login

--- clubroyal/www/js/WebService.js
+++ clubroyal/www/js/WebService.js
@@ -1,30 +1,38 @@
-var envia = function() {
-
+var canlogin = function() {
+    showLoading();
     jQuery.support.cors = true;
-
-
-    var wsUrl = "http://localhost/mapfreRe/public/wsdl/index/soap/getEstados";
-    var wsUrl = "http://programaatusalud.com/adccmdev/public/wsdl/index/soap/getComisionistas";
-    var soapRequest = '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://adccmdev.programaatusalud.com/wsdl/index/soap" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:enc="http://www.w3.org/2003/05/soap-encoding"><env:Body><ns1:getComisionistas env:encodingStyle="http://www.w3.org/2003/05/soap-encoding"/></env:Body></env:Envelope>';
-
+    var wsUrl = "http://programaatusalud.com/adccmdev/public/wsdl/index/soap/getCcmClienteInfo";
+//    var wsUrl = "http://localhost/adccmdev/public/wsdl/index/soap/getCcmClienteInfo";
+    var soapRequest = '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://localhost/adccmdev/public/wsdl/index/soap" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:enc="http://www.w3.org/2003/05/soap-encoding"><env:Body><ns1:getCcmClienteInfo env:encodingStyle="http://www.w3.org/2003/05/soap-encoding"><numeroCliente xsi:type="xsd:string">T301</numeroCliente><psw xsi:type="xsd:string">VDBSVk0wNUVZekE9</psw></ns1:getCcmClienteInfo></env:Body></env:Envelope>';
     $.ajax({
         type: "POST",
         url: wsUrl,
         contentType: "application/soap+xml",
         dataType: "xml",
-        data: soapRequest,
+        data: soapRequest,       
         success: processSuccess,
         error: processError
     });
 
-
     function processSuccess(data, status, req) {
-        alert(status);
-        if (status == "success")
-            $("#response").text($(req.responseXML).find("HelloResult").text());
+        if (status === "success") {
+            var response = $(req.responseXML).find("return").text();
+            var json = jQuery.parseJSON(response);
+            if (json.login) {
+                $("#response").text($.base64.encode("1234"));
+            }
+        }
     }
+
     function processError(data, status, req) {
-        console.log(data);
-        alert(req.responseText + " " + status);
+        alert("No se pudo establecer la conexión con el servidor");
+    }
+
+    function showLoading() {
+        $("#loading").click();
+    }
+
+    function hideLoading() {
+        $("#loading");
     }
 };
