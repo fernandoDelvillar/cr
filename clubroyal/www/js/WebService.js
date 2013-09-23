89d310fe6c57ddb35d15695481dc90a43d73bfa8
Davidg <Davidg@192.168.1.180>
2013-09-23 16:22:01 -0500

    integración con WS

--- clubroyal/www/js/WebService.js
+++ clubroyal/www/js/WebService.js
@@ -4,7 +4,7 @@ var canlogin = function() {
 
         $.mobile.allowCrossDomainPages = true;
         $.support.cors = true;
-        var wsUrl = "http://programaatusalud.com/adccmdev/public/wsdl/index/soap/getCcmClienteInfo";
+        var wsUrl = "http://programaatusalud.com/adccmdev/public/wsdl/index/soap#getCcmClienteInfo";
         //    var wsUrl = "http://localhost/adccmdev/public/wsdl/index/soap/getCcmClienteInfo";
         var soapRequest = '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://programaatusalud.com/adccmdev/public/wsdl/index/soap" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:enc="http://www.w3.org/2003/05/soap-encoding"><env:Body><ns1:getCcmClienteInfo env:encodingStyle="http://www.w3.org/2003/05/soap-encoding"><numeroCliente xsi:type="xsd:string">T301</numeroCliente><psw xsi:type="xsd:string">VDBSVk0wNUVZekE9</psw></ns1:getCcmClienteInfo></env:Body></env:Envelope>';
         $.ajax({
