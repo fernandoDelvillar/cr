38f0d85e22cbe224f77eadcc4002209add2e032c
Davidg <Davidg@192.168.1.180>
2013-09-23 09:27:35 -0500

    integración con WS

--- clubroyal/www/js/WebService.js
+++ clubroyal/www/js/WebService.js
@@ -1,32 +1,31 @@
 var canlogin = function() {
-    showLoading();
     jQuery.support.cors = true;
     var wsUrl = "http://adccmdev.programaatusalud.com/wsdl/index/soap/getCcmClienteInfo";
-//    var wsUrl = "http://localhost/adccmdev/public/wsdl/index/soap/getCcmClienteInfo";
+    var wsUrl = "http://localhost/adccmdev/public/wsdl/index/soap/getCcmClienteInfo";
     var soapRequest = '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://localhost/adccmdev/public/wsdl/index/soap" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:enc="http://www.w3.org/2003/05/soap-encoding"><env:Body><ns1:getCcmClienteInfo env:encodingStyle="http://www.w3.org/2003/05/soap-encoding"><numeroCliente xsi:type="xsd:string">T301</numeroCliente><psw xsi:type="xsd:string">VDBSVk0wNUVZekE9</psw></ns1:getCcmClienteInfo></env:Body></env:Envelope>';
     $.ajax({
+        async: false,
         type: "POST",
         url: wsUrl,
         contentType: "application/soap+xml",
         dataType: "xml",
-        data: soapRequest,       
+        data: soapRequest,
         success: processSuccess,
         error: processError
     });
 
     function processSuccess(data, status, req) {
         $("#response").text(data);
-         alert(status);
+        alert(status);
         if (status === "success") {
             var response = $(req.responseXML).find("return").text();
-            alert(response);
-            alert("davichos");
             $("#response").text(response);
             var json = jQuery.parseJSON(response);
+            alert(json.login.toString());
             if (json.login) {
                 $("#response").text($.base64.encode("1234"));
             }
-        }else{
+        } else {
             alert(status);
         }
     }
