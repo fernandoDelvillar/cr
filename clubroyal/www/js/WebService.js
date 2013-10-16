689fb3c08158df6eeff670381f9372784db86431
EFlores <EFlores@DESARROLLO_APP.adventa.local>
2013-10-16 14:17:41 -0500

    Cambios Css min-width estructure.css

--- clubroyal/www/js/WebService.js
+++ clubroyal/www/js/WebService.js
@@ -1,6 +1,6 @@
 
-var wsUrl = "http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap";
-//var wsUrl = "http://localhost/adccmdev/public/wsdl/index/soap";
+//var wsUrl = "http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap";
+var wsUrl = "http://localhost/adccmdev/public/wsdl/index/soap";
 $.mobile.allowCrossDomainPages = true;
 $.support.cors = true;
 
