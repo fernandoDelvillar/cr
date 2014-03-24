ec750ab94b4d28c9c4f50c659a0cce3d00de259d
fernandoDelvillar <fernando.delvillar@adventa.mx>
2014-03-24 14:18:40 -0600

    Update BdLocal.js

--- clubroyal/www/js/BdLocal.js
+++ clubroyal/www/js/BdLocal.js
@@ -329,7 +329,7 @@ var errorSql = function(error) {
 //    alert("No se puede inicializar la App");
     console.log(error);
     if (error.code == error.DATABASE_ERR)
-        alert('nasty database error');
+        alert(error);
 };
 var creaDBSuccess = function() {
     return true;
